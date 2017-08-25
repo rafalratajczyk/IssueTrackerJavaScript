71848080b216b9b8b98f5ba1925f718ac0d4b17f
Rafał Ratajczyk <rafalratajczyk940@gmail.com>
2017-08-25 12:25:51 +0200

    status closed

--- main.js
+++ main.js
@@ -31,6 +31,20 @@ function saveIssue(e) {
     e.preventDefault();
 }
 
+function setStatusClosed(id) {
+    var issues = JSON.parse(localStorage.getItem('issues'));
+
+    for (var i = 0; i < issues.length; i++) {
+        if (issues[i].id === id) {
+            issues[i].status = 'Closed';
+        }
+    }
+
+    localStorage.setItem('issues', JSON.stringify(issues));
+
+    fetchIssues();
+}
+
 function fetchIssues() {
     var issues = JSON.parse(localStorage.getItem('issues'));
     var issuesList = document.getElementById('issuesList');
