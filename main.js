0b7b8de173c709d4750bfceba8ee81d73daa815c
Rafał Ratajczyk <rafalratajczyk940@gmail.com>
2017-08-25 12:15:24 +0200

    save issue

--- main.js
+++ main.js
@@ -1,3 +1,30 @@
+document.getElementById('issueInputForm').addEventListener('submit', saveIssue);
+
+function saveIssue(e) {
+    var issueDesc = document.getElementById('issueDescInput').value;
+    var issueSev = document.getElementById('issueSevInput').value;
+    var issueAssignedTo = document.getElementById('issueAssignedToInput').value;
+    var issueId = chance.guid();
+    var issueStatus = 'Open';
+    var issue = {
+        id: issueId,
+        description: issueDesc,
+        severity: issueSev,
+        assignedTo: issueAssignedTo,
+        status: issueStatus
+    };
+
+    if (localStorage.getItem('issues') == null) {
+        var issues = [];
+        issues.push(issue);
+        localStorage.setItem('issues', JSON.stringify(issues));
+    } else {
+        var issues = JSON.parse(localStorage.getItem('issues'));
+        issues.push(issue);
+        localStorage.setItem('issues', JSON.stringify(issues));
+    }
+}
+
 function fetchIssues() {
     var issues = JSON.parse(localStorage.getItem('issues'));
     var issuesList = document.getElementById('issuesList');
